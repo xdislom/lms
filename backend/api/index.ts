1d76108b2af296724ec4b2ae0d6c8df18212868f
xdislom <islomiskandarov1118@gmail.com>
2026-09-02 16:20:57 +0500

    fix: backend serverless config va prisma integratsiyasi to'g'rilandi

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
-import { AppModule } from 'src/app.module';
 
 const server = express();
 
